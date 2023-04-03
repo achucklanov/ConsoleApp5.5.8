6f63f9ab34284fedaeda5c8d61a684905d923500
AndreyChuck <a.chucklanov@yandex.ru>
2023-04-03 21:56:55 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp5.5.8/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp5._5._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PowerUp(3,3));
+        }
+        private static int PowerUp(int N, byte pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (pow == 1)
+                {
+                    return N;
+                }
+                else
+                {
+                    return N * PowerUp(N, pow - 1);
+
+                }
+
+            }
+
+        }
+
+
+
+    }
+}
